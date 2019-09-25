90abd12a6ee69c374743cc2cafaf2af071a5502b
nick010001110 <a.i.001@mail.ru>
2019-09-25 15:53:36 +0300

    separated reducers
    Added eparated reducers for better readability and code structure

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import rootReducer from './reducers/rootReducer'
+import popularReducer from "./reducers/popular";
+import trendingReducer from "./reducers/trending";
+import {combineReducers } from 'redux';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(combineReducers({popular: popularReducer, trending: trendingReducer}), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
